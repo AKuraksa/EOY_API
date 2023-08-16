e1d4b1b6bc93954ac2d98151fad9e6e2232d68b9
jan korizek <tomas.dohnal46@seznam.cz>
2023-08-16 21:15:08 +0200

    DOHNY- Database - model

---
+++ EOY API/Tables/Login.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace EOY_API.Tables
+
+{
+    public class Login
+    {
+       
+        int id { get; set; }
+
+        
+        string username { get; set; }
+        
+        string password { get; set; }
+       
+        string email { get; set; }
+      
+        string First_name { get; set; }
+     
+        string Last_name { get; set; }
+    }
+}
---
+++ EOY API/Tables/Workplace.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace EOY_API.Tables
+{
+    public class Workplace
+    {
+        int ID { get; set; }
+        string Name_Workplace { get; set; }
+        string IP { get; set; }
+        string MAC { get; set; }
+        string STATE { get; set; }
+        string Name_device { get; set; }
+        
+        string GetHELP { get; set; }
+        string GetINFO { get; set; }
+     
+
+
+    }
+}
