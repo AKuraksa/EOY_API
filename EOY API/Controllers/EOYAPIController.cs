cfdffa326bf5b131b4aec0ccc62f742ceb6ac7bb
AKuraksa <andrej.kuraksa@seznam.cz>
2023-08-16 21:29:44 +0200

    Kury - commit

---
+++ EOY API/Controllers/EOYAPIController.cs
@@ -0,0 +1,21 @@
+﻿using EOY_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EOY_API.Controllers
+{
+    [Route("api/EOYAPI")]
+    [ApiController]
+    public class EOYAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<EOY> GetEOYs()
+        {
+            return new List<EOY>
+            {
+                new EOY{Id=1}
+            };
+        }
+        [HttpPost]
+        public 
+    }
+}
---
+++ EOY API/Models/EOY.cs
@@ -0,0 +1,7 @@
+﻿namespace EOY_API.Models
+{
+    public class EOY
+    {
+        public int Id { get; set; }
+    }
+}
